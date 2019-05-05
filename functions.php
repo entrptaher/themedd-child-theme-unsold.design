8972513dba01bcf5a3e6d07c07db6c018d3c26cd
entrptaher@gmail.com <entrptaher@gmail.com>
2019-05-05 14:44:31 +0600

    add file and description meta

--- functions.php
+++ functions.php
@@ -85,7 +85,8 @@ function vanila_themedd_edd_content($download_id)
     
     echo '<b>Files included:</b> <span class="file-formats">AI, PNG, PDF, SVG</span>';
 
-    echo '<b>Description:</b>';
+    echo '<b class="description-prefix">Description:</b>';
+
     echo the_content();
 
     $categories = themedd_edd_download_categories($download_id);
