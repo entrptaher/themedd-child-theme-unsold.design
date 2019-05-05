508388ec9445fcf3caa0812bcca723b7dda973b2
entrptaher@gmail.com <entrptaher@gmail.com>
2019-05-05 14:56:28 +0600

    Add quotes around the quotes collection

--- single.php
+++ single.php
@@ -5,49 +5,49 @@
 
 get_header();
 
-    # Random quotes
-    quotescollection_quote( array( 'ajax_refresh' => false, 'char_limit' => 300 ) );
+# Random quotes
+quotescollection_quote(array( 'ajax_refresh' => false, 'char_limit' => 300 ));
 
 ?>
 
-<?php do_action( 'themedd_single_start' ); ?>
+<?php do_action('themedd_single_start'); ?>
 
 <div class="content-wrapper<?php echo themedd_wrapper_classes(); ?>">
     <div id="primary" class="content-area<?php echo themedd_primary_classes(); ?>">
     	<main id="main" class="site-main" role="main">
 
 		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+        // Start the loop.
+        while (have_posts()) : the_post();
 
-			// Include the single post content template.
-			get_template_part( 'template-parts/content', 'single' );
+            // Include the single post content template.
+            get_template_part('template-parts/content', 'single');
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
+            // If comments are open or we have at least one comment, load up the comment template.
+            if (comments_open() || get_comments_number()) {
+                comments_template();
+            }
 
-			if ( is_singular( 'attachment' ) ) {
-				// Parent post navigation.
-				the_post_navigation( array(
-					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'themedd' ),
-				) );
-			} elseif ( is_singular( 'post' ) ) {
-				// Previous/next post navigation.
-				the_post_navigation( array(
-					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'themedd' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Next post:', 'themedd' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'themedd' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Previous post:', 'themedd' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-				) );
-			}
+            if (is_singular('attachment')) {
+                // Parent post navigation.
+                the_post_navigation(array(
+                    'prev_text' => _x('<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'themedd'),
+                ));
+            } elseif (is_singular('post')) {
+                // Previous/next post navigation.
+                the_post_navigation(array(
+                    'next_text' => '<span class="meta-nav" aria-hidden="true">' . __('Next', 'themedd') . '</span> ' .
+                        '<span class="screen-reader-text">' . __('Next post:', 'themedd') . '</span> ' .
+                        '<span class="post-title">%title</span>',
+                    'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __('Previous', 'themedd') . '</span> ' .
+                        '<span class="screen-reader-text">' . __('Previous post:', 'themedd') . '</span> ' .
+                        '<span class="post-title">%title</span>',
+                ));
+            }
 
-			// End of the loop.
-		endwhile;
-		?>
+            // End of the loop.
+        endwhile;
+        ?>
 
 	</main>
 
